3fce05f485fe27fd2f59aa8f38a669d16e83316e
apple <appleapple@apples-MacBook-Air.local>
2026-01-23 20:26:42 +0530

    test code for sonarqube

--- stdentappsonaqube.groovy
+++ stdentappsonaqube.groovy
@@ -1,12 +1,11 @@
-psh '''mvn clean install sonar:sonar   -Dsonar.projectKey=studentUI-app   -Dsonar.host.url=http://13.63.63.140:9000   -Dsonar.login=2298879029859d00ce7dfad6435ceee9631ca5d3   -Dsonar.java.binaries=target/classes
-'''ipeline {
+pipeline {
     agent { label 'slave' }
     stages {
         stage('git-pull-stage') {
             steps {
                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
             }
-       }
+        }
          stage('build-stage') {
             steps {
                 sh '/opt/maven/bin/mvn clean package'
@@ -14,19 +13,10 @@ psh '''mvn clean install sonar:sonar   -Dsonar.projectKey=studentUI-app   -Dsona
         }
          stage('test-stage') {
             steps {
-                sh '''/opt/maven/bin/mvn clean install sonar:sonar   -Dsonar.projectKey=studentUI-app   -Dsonar.host.url=http://172.31.10.19:9000   -Dsonar.login=2298879029859d00ce7dfad6435ceee9631ca5d3   -Dsonar.java.binaries=target/classes
+                sh '''mvn clean install sonar:sonar   -Dsonar.projectKey=studentUI-app   -Dsonar.host.url=http://172.31.10.19:9000   -Dsonar.login=2298879029859d00ce7dfad6435ceee9631ca5d3   -Dsonar.java.binaries=target/classes
 '''
             }
         }
-         stage('Quality_Gate') {
-            steps {
-                timeout(10) {
-   
-            }
-                waitForQualityGate true
-            }
-        }
-
          stage('deploy-stage') {
             steps {
                 echo 'code deploy sucessfully'
