f70129063987d7e07683cc830dd1d8e06e02e23d
Samiksha-Kamble39 <samikshakamble115@gmail.com>
2026-02-06 13:48:43 +0530

    Create only-quality-gate.groovy

---
+++ only-quality-gate.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent { label 'slave' }
+    stages {
+        stage('git-pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+         stage('build-stage') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+         stage('test-stage') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar',credentialsId: 'cred1-sonar') {
+                 sh ' /opt/maven/bin/mvn clean verify sonar:sonar '
+            }
+
+            }
+        }
+         stage('Quality_Gate') {
+            steps {
+                timeout(2) {
+   
+            }
+                waitForQualityGate true
+            }
+        }
+         stage('deploy-stage') {
+            steps {
+                echo 'code deploy sucessfully'
+            }
+        }
+    }
+}
