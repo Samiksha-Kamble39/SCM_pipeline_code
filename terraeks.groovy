a1ad9fdca75a82fc786458ab22d5543bf741756a
Samiksha-Kamble39 <samikshakamble115@gmail.com>
2026-02-06 19:42:52 +0530

    Update terraeks.groovy

--- terraeks.groovy
+++ terraeks.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/Samiksha-Kamble39/Terraform.git'
+                git branch: 'main', url: 'https://github.com/Samiksha-Kamble39/terraform.git'
             }
         }
         stage('Test') {
